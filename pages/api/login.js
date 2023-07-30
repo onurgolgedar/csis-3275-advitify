4a93d3c0e0c2fd339dc3b7c32efcbf409d89b0d2
Onur Gölgedar <b.onur.golgedar@gmail.com>
2023-07-30 12:46:02 -0700

    Simple API Login + Database Setup
    Simple API Login function has been added after Advitify database setup.

---
+++ pages/api/login.js
@@ -0,0 +1,43 @@
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
+
+export default async function handler(req, res) {
+  if (req.method === "POST") return POST(req, res);
+  else if (req.method === "GET") return GET(req, res);
+  else res.status(405).end();
+}
+
+export async function POST(req, res) {
+  const { username, password } = req.body;
+
+  if (await login(username, password)) {
+    return res.status(200).json({
+      message: "Login successful!",
+    });
+  } else {
+    return res.status(401).json({
+      message: "Invalid credentials.",
+    });
+  }
+}
+
+export async function GET(req, res) {}
+
+const prisma = new PrismaClient();
+
+async function login(username, password) {
+  const user = await prisma.users.findFirst({
+    where: {
+      username: username
+    }
+  });
+
+  if (!user)
+    return false;
+
+  const isValidPassword = bcrypt.compare(password, user.passwordHash);
+  if (!isValidPassword)
+    return false;
+
+  return user;
+}
--- pages/layout.jsx
+++ pages/layout.jsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import NavBar from '@/app/components/NavBar'
-import Footer from '@/app/components/Footer'
+import NavBar from '@/pages/components/NavBar'
+import Footer from '@/pages/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
