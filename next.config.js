e8fa7a38fe9db4fe96d5bac1c4e358323a9ce590
Shingo Tennichi <sngsng.ttt@gmail.com>
2023-08-04 16:01:34 -0700

    add headers for zoom meeting sdk

--- next.config.js
+++ next.config.js
@@ -2,7 +2,25 @@
 const nextConfig = {
   images: {
     domains: ['images.unsplash.com']
-  }
+  },
+  // for zoom meeting to allow sharing screen
+  async headers() {
+    return [
+      {
+        source: '/online-session/:meetingId',
+        headers: [
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+        ],
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
