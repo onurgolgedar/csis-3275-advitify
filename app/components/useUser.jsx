dc6f3b687d2c3c645b1c64d35559eb081ad1284a
Onur Gölgedar <b.onur.golgedar@gmail.com>
2023-08-05 00:28:12 -0700

    dev

--- app/components/useUser.jsx
+++ app/components/useUser.jsx
@@ -7,19 +7,18 @@ import jwt from "jsonwebtoken";
 export function useUser() {
   const user = useContext(UserContext);
 
-  console.log('Level1:', user);
   if (user) {
     const token = user.user?.data.token;
-    console.log('Level2: ', token);
     // const decoded = jwt.verify(token, process.env.NEXT_PUBLIC_JWT_SECRET, function (err, decoded) {
     //   if (err) {
-    //     console.log("error : ", err);
+    //     console.log("Error -> ", err);
     //   }
     // })
-    // console.log("Level3: ", decoded);
 
     // if(decoded) return user;
-    // else return user;
+    // else return null;
+    if (token == null)
+      return null;
   }
 
   return user;
