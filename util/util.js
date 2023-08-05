b08198e6e6c86d903d4a181f89b611d7f92fb0c8
Onur Gölgedar <b.onur.golgedar@gmail.com>
2023-08-05 00:25:00 -0700

    dev

--- util/util.js
+++ util/util.js
@@ -1,10 +1,40 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
 export function CreateResponse(success, data = null, message = null) {
-    return new Response(JSON.stringify({
-        success: success,
-        data: data,
-        message: message
-    }))
+  return new Response(
+    JSON.stringify({
+      success: success,
+      data: data,
+      message: message,
+    })
+  );
 }
 
+export function extractToken(req) {
+  const authHeader = req.headers.get("authorization");
+
+  if (!authHeader) return null;
+
+  const tokenParts = authHeader.split(" ");
+  if (tokenParts.length !== 2 || tokenParts[0] !== "Bearer") return null;
+
+  return tokenParts[1];
+}
+
+export async function validateToken(token) {
+  const prisma = new PrismaClient();
+  if (!token) return false;
+
+  const loginRecord = await prisma.login.findUnique({
+    where: {
+      token: token,
+    },
+  });
+
+  if (!loginRecord) return false;
+
+  if (loginRecord.expiryDate && loginRecord.expiryDate < new Date())
+    return false;
+
+  return true;
+}
