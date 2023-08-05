1aa5d291553f179926687478c6db9e2fa858e9fb
Onur Gölgedar <b.onur.golgedar@gmail.com>
2023-08-05 12:25:32 -0700

    sign up fix

--- app/api/registerClient/route.js
+++ app/api/registerClient/route.js
@@ -1,28 +1,27 @@
 import { PrismaClient } from "@prisma/client";
 import { createResponse, checkLogin } from "../../../util/util.js";
+import bcrypt from "bcrypt";
 
 export async function POST(req, res) {
   const prisma = new PrismaClient();
 
   try {
-    const checkLogin = checkLogin(req);
-    if (checkLogin != true)
-      return checkLogin;
-
     const { username, email, password } = await req.json();
 
+    var passwordHash = await bcrypt.hash(password, 8);
+    console.log(passwordHash);
     const newUser = await prisma.users.create({
       data: {
         username: username,
         email: email,
-        passwordHash: password,
+        passwordHash: passwordHash,
         userType: "2",
       },
     });
 
     return createResponse(true, newUser, "User has been created successfully.");
   } catch (e) {
-    console.log("-> Request failed");
+    console.log("-> Request failed" + e);
     return createResponse(false, null, e);
   } finally {
     prisma.$disconnect();
